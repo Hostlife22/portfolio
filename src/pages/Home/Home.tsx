1db4cd51de3747ee8b4819de0f28f8515c0267b2
Serafim Sen <sima.serafim2021@mail.ru>
2022-06-11 16:22:41 +0300

    fix: avatar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from 'react';
 import Typed from 'typed.js';
 import { Link, Row, Section } from '../../components';
+import { ImgAvatar } from '../../images';
 import styles from './Home.module.css';
 
 function Home() {
@@ -40,10 +41,7 @@ function Home() {
           <Link href="/cv">Open CV</Link>
         </div>
         <div className={styles.homeImg}>
-          <img
-            src="https://megakebabs.herokuapp.com/packs/media/images/user-72484b17d9f5677d9fa3d9b2363ef633.jpg"
-            alt="Avatar"
-          />
+          <img src={ImgAvatar} alt="Avatar" />
         </div>
       </Row>
     </Section>
